12b6f642991f6a88dd43f75e388fd55f900fe27e
Jacob Jonsson <jacobjjonsson@gmail.com>
2022-02-18 15:38:28 +0100

    Style button

--- src/app.tsx
+++ src/app.tsx
@@ -292,7 +292,11 @@ export function App() {
       onDragCancel={handleDragCancel}
     >
       <div className="max-w-4xl mx-auto p-4">
-        <button type="button" onClick={() => setShowModal({ open: true })}>
+        <button
+          type="button"
+          onClick={() => setShowModal({ open: true })}
+          className="border border-gray-300 rounded px-4 py-2 mb-4 w-full hover:bg-gray-50"
+        >
           Add item
         </button>
         {showModal.open && (
